d2b44f42c1301cf9c9dc32d5b192cf1c6898129e
Emily Zhang <emilyzhang918@gmail.com>
2024-06-08 19:55:36 -0700

    separate buttons row to exclude buttons from being draggable

--- vite-project/src/components/miniPropertyCard/MiniPropertyCard.jsx
+++ vite-project/src/components/miniPropertyCard/MiniPropertyCard.jsx
@@ -1,65 +1,61 @@
-import React, { useState } from 'react';
-import { useSortable } from '@dnd-kit/sortable';
-import { CSS } from '@dnd-kit/utilities';
-import './MiniPropertyCard.css';
-import Carousel from '../carousel/Carousel';
-import { Button } from '@mui/material';
+import React, { useState } from 'react'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import './MiniPropertyCard.css'
+import Carousel from '../carousel/Carousel'
+import { Button } from '@mui/material'
 
 function MiniPropertyCard(props) {
-  const { propertyInfo, likedFn, dislikedFn, displayPopup } = props;
-  const [allowDragging, setAllowDragging] = useState(true);
+  const { propertyInfo, likedFn, dislikedFn, displayPopup } = props
 
-  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
-    id: propertyInfo.houseID,
-    disabled: !allowDragging, // Disable sorting if allowDragging is false
-  });
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({
+      id: propertyInfo.houseID,
+    })
 
   const likeProperty = () => {
-    likedFn(propertyInfo.houseID);
-  };
+    likedFn(propertyInfo.houseID)
+  }
 
   const dislikeProperty = () => {
-    dislikedFn(propertyInfo.houseID);
-  };
+    dislikedFn(propertyInfo.houseID)
+  }
 
   const expandProperty = () => {
-    console.log('house ' + propertyInfo.houseID + ' was expanded!');
-    displayPopup();
-  };
-
-  const handleMouseDown = (event) => {
-    if (event.target.classList.contains('expand-info-button')) {
-      console.log("expand clicked")
-      setAllowDragging(false); // Disable dragging when clicking on the expand button
-    } else {
-      setAllowDragging(true); // Enable dragging for other elements
-    }
-  };
+    console.log('expand clicked through expandProperty')
+    console.log('house ' + propertyInfo.houseID + ' was expanded!')
+    displayPopup()
+  }
 
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
-  };
+  }
 
   return (
-    <li
-      className="property-card"
-      ref={setNodeRef}
-      style={style}
-      {...attributes}
-      {...listeners}
-      onMouseDown={handleMouseDown} // Add onMouseDown event handler
-    >
-      <div className="carousel-container">
-        <Carousel data={propertyInfo.images} size={{ width: null, height: null }} />
-      </div>
-      <div className="card-details">
-        <h3 className="house-title">{propertyInfo.title}</h3>
-        <div className="details-row">
-          <span className="rent">${propertyInfo.price}</span>
-          <span className="address">{propertyInfo.address}</span>
-          <span className="house-type">{propertyInfo.roomType}</span>
-        </div>
+    <>
+      <div className="property-card">
+        <li
+          ref={setNodeRef}
+          style={style}
+          {...attributes}
+          {...listeners}
+        >
+          <div className="carousel-container">
+            <Carousel
+              data={propertyInfo.images}
+              size={{ width: null, height: null }}
+            />
+          </div>
+          <div className="card-details">
+            <h3 className="house-title">{propertyInfo.title}</h3>
+            <div className="details-row">
+              <span className="rent">${propertyInfo.price}</span>
+              <span className="address">{propertyInfo.address}</span>
+              <span className="house-type">{propertyInfo.roomType}</span>
+            </div>
+          </div>
+        </li>
         <div className="buttons-row">
           <button
             className="circle-button cross-button"
@@ -67,7 +63,11 @@ function MiniPropertyCard(props) {
           >
             ✖
           </button>
-          <Button variant="contained" onClick={expandProperty} className="expand-info-button">
+          <Button
+            variant="contained"
+            onClick={expandProperty}
+            className="expand-info-button"
+          >
             Expand Info
           </Button>
           <button
@@ -78,8 +78,8 @@ function MiniPropertyCard(props) {
           </button>
         </div>
       </div>
-    </li>
-  );
+    </>
+  )
 }
 
-export default MiniPropertyCard;
+export default MiniPropertyCard
