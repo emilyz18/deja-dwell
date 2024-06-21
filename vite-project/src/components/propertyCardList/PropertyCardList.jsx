b40cb563ae1341ddd26ae3f7b67d2e22bc322397
jacksonliiii <jacksonli2001@hotmail.com>
2024-06-20 20:40:16 -0700

    run prettier

--- vite-project/src/components/propertyCardList/PropertyCardList.jsx
+++ vite-project/src/components/propertyCardList/PropertyCardList.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import {
   DndContext,
   DragOverlay,
@@ -10,14 +10,14 @@ import { SortableContext, rectSortingStrategy } from '@dnd-kit/sortable'
 import MiniPropertyCard from '../miniPropertyCard/MiniPropertyCard'
 import { ExpandedPropertyCard } from '../expandedPropertyCard/expandedPropertyCard'
 import './PropertyCardList.css'
-import { getPropertiesAsync } from '../../redux/properties/thunks.js';
-
+import { getPropertiesAsync } from '../../redux/properties/thunks.js'
 
 function PropertyCardList(props) {
-
-  const dispatch = useDispatch();
-  const propertiesList = useSelector((state) => state.properties.list);
-  const getPropertiesStatus = useSelector((state) => state.properties.getProperties);
+  const dispatch = useDispatch()
+  const propertiesList = useSelector((state) => state.properties.list)
+  const getPropertiesStatus = useSelector(
+    (state) => state.properties.getProperties
+  )
 
   const { propList } = props
   const [properties, setProperties] = useState(propList)
@@ -30,13 +30,11 @@ function PropertyCardList(props) {
 
   useEffect(() => {
     if (getPropertiesStatus === 'IDLE') {
-      dispatch(getPropertiesAsync());
+      dispatch(getPropertiesAsync())
     }
-  }, [getPropertiesStatus, properties, dispatch]);
+  }, [getPropertiesStatus, properties, dispatch])
 
-  console.log(propertiesList);
-
-    // TODO: to be implemented logic
+  // TODO: to be implemented logic
   const likedProperty = (id) => {
     console.log('house ' + id + ' was liked!')
 
@@ -47,7 +45,7 @@ function PropertyCardList(props) {
     console.log(properties)
   }
 
-    // TODO: to be implemented logic
+  // TODO: to be implemented logic
   const dislikedProperty = (id) => {
     console.log('house ' + id + ' was rejected!')
     const updatedProperties = properties.filter(
@@ -75,8 +73,9 @@ function PropertyCardList(props) {
 
   const handleDragEnd = (event) => {
     const { delta } = event
-    
-    if (delta.x > 200) { // determines the x coord to be dragged
+
+    if (delta.x > 200) {
+      // determines the x coord to be dragged
       likedProperty(activeId)
     } else if (delta.x < -200) {
       dislikedProperty(activeId)
@@ -88,23 +87,22 @@ function PropertyCardList(props) {
 
   const { setNodeRef: setLikeRef, isOver: isOverLike } = useDroppable({
     id: 'like-dropzone',
- 
-  });
-  
+  })
+
   const { setNodeRef: setDislikeRef, isOver: isOverDislike } = useDroppable({
     id: 'dislike-dropzone',
     onDragEnter: () => {
-      console.log('Draggable card entered the dislike dropzone');
+      console.log('Draggable card entered the dislike dropzone')
     },
     onDragLeave: () => {
-      console.log('Draggable card left the dislike dropzone');
+      console.log('Draggable card left the dislike dropzone')
     },
-  });
+  })
 
   // The code below was written with the help of ChatGPT 3.5 on Jun 8th
-  // Prompt: Give me some examples of dragging and dropping using the dnd kit. Then, use the 
+  // Prompt: Give me some examples of dragging and dropping using the dnd kit. Then, use the
   // dnd toolkit to incorporate drag and drop functionality on the miniProperty card + "this file".
-  // The generated code was adapted: I added place holders for dropzones and cards to be 
+  // The generated code was adapted: I added placeholders for drop zones and cards to be
   // conditionally displayed. I also wrote css myself tp suit my own needs
   return (
     <>
@@ -122,14 +120,16 @@ function PropertyCardList(props) {
               <span className="dropzone-icon">✖</span>
             </div>
           ) : (
-            <div className="dropzone-placeholder">
-            </div>
+            <div className="dropzone-placeholder"></div>
           )}
           <SortableContext items={properties} strategy={rectSortingStrategy}>
             <ul id="property-list" className="property-list">
-              {properties.map((property) => (
+              {properties.map((property) =>
                 property.houseID === activeId ? (
-                  <div key={property.houseID} className="placeholder-card"></div>
+                  <div
+                    key={property.houseID}
+                    className="placeholder-card"
+                  ></div>
                 ) : (
                   <MiniPropertyCard
                     key={property.houseID}
@@ -139,7 +139,7 @@ function PropertyCardList(props) {
                     displayPopup={() => displayPopup(property)}
                   />
                 )
-              ))}
+              )}
             </ul>
           </SortableContext>
           {isDragging ? (
@@ -150,8 +150,7 @@ function PropertyCardList(props) {
               <span className="dropzone-icon">✔</span>
             </div>
           ) : (
-            <div className="dropzone-placeholder">
-            </div>
+            <div className="dropzone-placeholder"></div>
           )}
         </div>
         <DragOverlay>
--- vite-project/src/components/userLogin/ProtectedRoute.jsx
+++ vite-project/src/components/userLogin/ProtectedRoute.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import App from '../../App.jsx';
+import React from 'react'
+import { Route, Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import App from '../../App.jsx'
 
 const ProtectedRoute = ({ element: Element, ...rest }) => {
-    const isAuthenticated = useSelector(state => state.user.isAuthenticated);
-    console.log(isAuthenticated);
-    return isAuthenticated ? <App /> : <Navigate to="/login" />;
-};
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated)
+  return isAuthenticated ? <App /> : <Navigate to="/login" />
+}
 
-export default ProtectedRoute;
+export default ProtectedRoute
