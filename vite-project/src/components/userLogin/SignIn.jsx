41760680627fc92cdd99827923f26819b6ebccd0
jacksonliiii <jacksonli2001@gmail.com>
2024-07-23 19:07:25 -0700

    Update authentication css

--- vite-project/src/components/userLogin/SignIn.jsx
+++ vite-project/src/components/userLogin/SignIn.jsx
@@ -1,119 +1,62 @@
-import React, { useState, useEffect } from 'react'
-import Avatar from '@mui/material/Avatar'
-import Button from '@mui/material/Button'
-import CssBaseline from '@mui/material/CssBaseline'
-import TextField from '@mui/material/TextField'
-import Link from '@mui/material/Link'
-import Grid from '@mui/material/Grid'
-import Box from '@mui/material/Box'
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
-import Typography from '@mui/material/Typography'
-import Container from '@mui/material/Container'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { useSelector, useDispatch } from 'react-redux'
-import { signInAsync } from '../../redux/user/thunks'
-import { useNavigate } from 'react-router-dom'
-import { theme } from '../themes.jsx'
-
-// COPY RIGHT: THIS PAGE CONTENT IS COPY AND MODIFY FROM https://github.com/mui/material-ui/blob/v5.15.20/docs/data/material/getting-started/templates/sign-in/SignIn.js
-// TODO remove, this demo shouldn't need to reset the theme.
-
-const defaultTheme = createTheme()
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { signInAsync } from '../../redux/user/thunks';
+import { useNavigate } from 'react-router-dom';
+import './styles.css';
 
 export default function SignIn() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const isAuth = useSelector((state) => state.user.isAuthenticated)
-  const isLandlord = useSelector((state) => state.user.isLandlord)
-  const isTenant = useSelector((state) => state.user.isTenant)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.user.isAuthenticated);
+  const isLandlord = useSelector((state) => state.user.isLandlord);
+  const isTenant = useSelector((state) => state.user.isTenant);
+
   const handleSubmit = (event) => {
-    event.preventDefault()
-    const data = new FormData(event.currentTarget)
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
     const user = {
       Email: data.get('email'),
       Password: data.get('password'),
-    }
-    dispatch(signInAsync(user))
-  }
+    };
+    dispatch(signInAsync(user));
+  };
 
   useEffect(() => {
-    if (isAuth === true) {
-      console.log('Authorization successful')
+    if (isAuth) {
       if (isLandlord) {
-        navigate('/landlordAccount/applicants')
-        console.log('Heading to landlord dashboard...')
+        navigate('/landlordAccount/applicants');
       } else if (isTenant) {
-        navigate('/tenantAccount/matches')
-        console.log('Heading to tenant dashboard...')
+        navigate('/tenantAccount/matches');
       }
     }
-  }, [isAuth, dispatch, isLandlord, isTenant, navigate])
+  }, [isAuth, dispatch, isLandlord, isTenant, navigate]);
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              color="jet"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              color="jet"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              color="jet"
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              <span>Sign In</span>
-            </Button>
-            <Grid container>
-              <Grid item>
-                <Link href="/register" variant="body2">
-                  <span>Don't have an account? Sign Up</span>
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  )
+    <div className="auth-container">
+      <div className="auth-wrapper">
+        <div className="auth-black-block">
+          <h1 className="auth-title">Renti.</h1>
+        </div>
+        <div className="auth-form-container">
+          <div className="auth-form-box">
+            <h2 className="auth-subtitle">Sign In</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="auth-form-group">
+                <label htmlFor="email" className="auth-label">Email Address</label>
+                <input id="email" name="email" required className="auth-input" />
+              </div>
+              <div className="auth-form-group">
+                <label htmlFor="password" className="auth-label">Password</label>
+                <input id="password" name="password" required className="auth-input" />
+              </div>
+              <button type="submit" className="auth-button">Sign In</button>
+            </form>
+            <div className="auth-link">
+              <a href="/register">{`Don't have an account? Sign Up`}</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- vite-project/src/components/userLogin/SignUp.jsx
+++ vite-project/src/components/userLogin/SignUp.jsx
@@ -1,164 +1,86 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import Avatar from '@mui/material/Avatar'
-import Button from '@mui/material/Button'
-import CssBaseline from '@mui/material/CssBaseline'
-import TextField from '@mui/material/TextField'
-import Link from '@mui/material/Link'
-import Grid from '@mui/material/Grid'
-import Box from '@mui/material/Box'
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
-import Typography from '@mui/material/Typography'
-import Container from '@mui/material/Container'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { signUpAsync } from '../../redux/user/thunks'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import Select from '@mui/material/Select'
-
-// COPY RIGHT: THIS PAGE CONTENT IS COPY AND MODIFY FROM https://github.com/mui/material-ui/blob/v5.15.20/docs/data/material/getting-started/templates/sign-up/SignUp.js
-// TODO remove, this demo shouldn't need to reset the theme.
-
-const defaultTheme = createTheme()
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { signUpAsync } from '../../redux/user/thunks';
+import './styles.css';
 
 export default function SignUp() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const isAuth = useSelector((state) => state.user.isAuthenticated)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.user.isAuthenticated);
 
-  const [accountType, setAccountType] = React.useState('')
+  const [accountType, setAccountType] = useState('');
 
   const handleChange = (event) => {
-    setAccountType(event.target.value)
-  }
+    setAccountType(event.target.value);
+  };
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    const data = new FormData(event.currentTarget)
-    console.log({
-      userName: data.get('userName'),
-      email: data.get('email'),
-      password: data.get('password'),
-      accountType: accountType,
-    })
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
     const user = {
       UserName: data.get('userName'),
       Email: data.get('email'),
       Password: data.get('password'),
       accountType: accountType,
-    }
+    };
     if (!user.UserName || !user.Email || !user.Password || !user.accountType) {
-      console.log('not allow for empty field')
+      console.log('Not allowed for empty field');
     } else {
-      dispatch(signUpAsync(user))
+      dispatch(signUpAsync(user));
     }
-  }
+  };
 
   useEffect(() => {
-    if (isAuth == true) {
-      console.log('Auth!!')
-      // fromSignUp state is for displaying the warning after sign up
+    if (isAuth) {
       if (accountType === 'Landlord') {
         navigate('/landlordAccount/profile', {
           state: { fromSignUp: 'Landlord' },
-        })
+        });
       } else if (accountType === 'Tenant') {
-        navigate('/tenantAccount/profile', { state: { fromSignUp: 'Tenant' } })
+        navigate('/tenantAccount/profile', { state: { fromSignUp: 'Tenant' } });
       }
     }
-  }, [isAuth, dispatch, navigate, accountType])
+  }, [isAuth, dispatch, navigate, accountType]);
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
-          >
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  autoComplete="userName"
-                  name="userName"
-                  required
-                  fullWidth
-                  id="userName"
-                  label="User Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <InputLabel id="account-type">Account Type</InputLabel>
-                <Select
-                  required
-                  labelId="account-type"
-                  id="account-type"
-                  value={accountType}
-                  label="Account Type"
-                  onChange={handleChange}
-                >
-                  <MenuItem value={'Landlord'}>Landlord</MenuItem>
-                  <MenuItem value={'Tenant'}>Tenant</MenuItem>
-                </Select>
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/login" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  )
+    <div className="auth-container">
+      <div className="auth-wrapper">
+        <div className="auth-black-block">
+          <h1 className="auth-title">Renti.</h1>
+        </div>
+        <div className="auth-form-container">
+          <div className="auth-form-box">
+            <h2 className="auth-subtitle">Sign Up</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="auth-form-group">
+                <label htmlFor="userName" className="auth-label">User Name</label>
+                <input id="userName" name="userName" required className="auth-input" />
+              </div>
+              <div className="auth-form-group">
+                <label htmlFor="email" className="auth-label">Email Address</label>
+                <input id="email" name="email" required className="auth-input" />
+              </div>
+              <div className="auth-form-group">
+                <label htmlFor="password" className="auth-label">Password</label>
+                <input id="password" name="password" required className="auth-input" />
+              </div>
+              <div className="auth-form-group">
+                <label htmlFor="account-type" className="auth-label">Account Type</label>
+                <select id="account-type" value={accountType} onChange={handleChange} required className="auth-select">
+                  <option value="" disabled>Select an account type</option>
+                  <option value="Landlord">Landlord</option>
+                  <option value="Tenant">Tenant</option>
+                </select>
+              </div>
+              <button type="submit" className="auth-button">Sign Up</button>
+            </form>
+            <div className="auth-link">
+              <a href="/login">Already have an account? Sign in</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
