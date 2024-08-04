e40438fc1f9f960f98d805bdd035b15e9b4d5f20
unknown <emilyzhang918@gmail.com>
2024-08-03 22:29:20 -0700

    fix css to align content to top

--- vite-project/src/components/propertyCardList/PropertyCardList.jsx
+++ vite-project/src/components/propertyCardList/PropertyCardList.jsx
@@ -453,7 +453,6 @@ function PropertyCardList({ searchMode }) {
                   }
                 />
               )}
-              while dragging
             </DragOverlay>
           </DndContext>
           {popupVisible && (
